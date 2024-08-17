d5c9eea8a5ea4e6ddeef3aaad87fc2d5b5e67997
genirohtea <genirohtea@gmail.com>
2024-08-16 20:59:08 -0700

    feat(ansible): filled in ansible collection template for google drive mounting

---
+++ install_google_drive_mount.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+# https://betterdev.blog/minimal-safe-bash-script-template/
+set -Eeuxo pipefail
+
+# script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+upsearch() {
+  slashes=${PWD//[^\/]/}
+  directory="$PWD"
+  for ((n = ${#slashes}; n > 0; --n)); do
+    test -e "$directory/$1" && break
+    directory="$directory/.."
+  done
+  ansible_dir=$(python3 -c "import os;print(os.path.abspath('${directory}'))")
+  echo "${ansible_dir}"
+}
+
+ansible_dir=$(upsearch ".ansible_root_signpost")
+echo "Identified ansible root directory as ${ansible_dir}"
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-t <tags>] [-r] [-i] [-s <SHA>] -H <host>
+
+This script runs the Ansible playbook for mounting Google Drive
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-t, --tags      Specify Ansible tags to run specific tasks
+-H, --host      Specify the host from the Ansible inventory to run the playbook on
+EOF
+  exit
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+use_ssh_pass='false'
+tags=''
+parse_params() {
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose)
+      set -x
+      verbosity="-vvv"
+      ;;
+    -t | --tags)
+      tags="${2-}"
+      shift
+      ;;
+    -s | --use_ssh_pass) use_ssh_pass='true' ;; # example flag
+    -H | --host)
+      host="${2-}"
+      shift
+      ;;
+
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  # check required params and arguments
+  [[ -z "${use_ssh_pass-}" ]] && die "Missing required parameter: use_ssh_pass"
+  [[ -z "${host-}" ]] && die "Missing required parameter: host"
+
+  return 0
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+parse_params "$@"
+
+msg "Read parameters:"
+msg "- arguments: ${args[*]-}"
+msg "- use_ssh_pass: ${use_ssh_pass}"
+
+read -p "Please confirm arguments: " -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  msg "User accepted arguments"
+else
+  die "User rejected arguments"
+fi
+
+msg "Running playbook on ${host}"
+
+if [[ -n "${tags-}" ]]; then
+  tags="--tags ${tags}"
+fi
+
+if [[ "${use_ssh_pass}" == "true" ]]; then
+  ssh_pass="--ask-pass"
+fi
+
+pushd "${ansible_dir}"
+
+# shellcheck disable=SC2086
+ansible-playbook main.yml ${ssh_pass:-} ${verbosity:-} ${tags} --extra-vars '{}' --limit "${host}" -i inventory/hosts.yaml
+
+popd
